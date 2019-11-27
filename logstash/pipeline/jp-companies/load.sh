f552a6d6eb0dc4a47d9bb9f25d71e25f92d64888
yuyc <cleverforever@gmail.com>
2019-11-27 12:25:45 +0900

    make it background running an add the log

--- logstash/pipeline/jp-companies/load.sh
+++ logstash/pipeline/jp-companies/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-logstash -f logstash_company.conf --path.data=/usr/share/logstash/pipeline/
+logstash -f logstash_company.conf --path.data=/usr/share/logstash/pipeline/ >$(date +'%Y%m%d').log 2>&1 &
